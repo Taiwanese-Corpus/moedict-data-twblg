9246cb7a2fb1b3b3c9826a96053b55d42839c86e
leo811006 <leo811006@yahoo.com.tw>
2014-02-25 17:54:37 +0800

    123

--- test/test3/__init__.py
+++ test/test3/__init__.py
@@ -28,36 +28,37 @@ def excel_table(file , colnameindex=0, by_index=0):
 
 class MyHTMLParser(HTMLParser):
     Output = []
-    counter = 0           
+    counter = 0 
+    final = set()          
     def handle_data(self, data):                                        
         
         self.counter += 1                  
-        print(self.counter)        
-        self.Output.append(data)
-        final = list(set(self.Output))
-        final.sort(key = self.Output.index)   
-        print(final)              
+        print(self.counter)
+        if data not in set(self.final):        
+            self.Output.append(data)
+            self.final = list(set(self.Output))
+            self.final.sort(key = self.Output.index)   
+        print(self.final)              
         
              
            
 def main():
-   result = excel_table(r'D:\JAVA\workspace\test\twblg_data_20131230\例句.xls')|excel_table(r'D:\JAVA\workspace\test\twblg_data_20131230\釋義.xls')    
-   counter = 0
+   result = excel_table(r'E:\Documents and Settings\LEO\git\temp\test\twblg_data_20131230\例句.xls')|excel_table(r'E:\Documents and Settings\LEO\git\temp\test\twblg_data_20131230\釋義.xls')    
    
-   for i in range(len(result)):      
-       FirstWord = urllib.request.quote(result.pop())        
+   
+   for i in result:      
+       FirstWord = urllib.request.quote(i)        
        urlx = "http://twblg.dict.edu.tw/holodict_new/searchSuggest.jsp?sample="+FirstWord+"&querytarget=2"
        sock = urllib.request.urlopen(urlx)
        parser = MyHTMLParser(strict=False)     
        parser.feed(sock.read().decode("utf8").strip())
            
        if parser.counter == 10:
-           temp = excel_table(r'D:\JAVA\workspace\test\twblg_data_20131230\例句.xls')|excel_table(r'D:\JAVA\workspace\test\twblg_data_20131230\釋義.xls')                            
+           temp = excel_table(r'E:\Documents and Settings\LEO\git\temp\test\twblg_data_20131230\例句.xls')|excel_table(r'E:\Documents and Settings\LEO\git\temp\test\twblg_data_20131230\釋義.xls')                              
            
            for i in range(len(temp)):
                SecondWord = urllib.request.quote(temp.pop())                
                urlx = "http://twblg.dict.edu.tw/holodict_new/searchSuggest.jsp?sample="+FirstWord+SecondWord+"&querytarget=2"
-               print(urlx)
                sock = urllib.request.urlopen(urlx)
                parser = MyHTMLParser(strict=False)
                parser.feed(sock.read().decode("utf8").strip())
