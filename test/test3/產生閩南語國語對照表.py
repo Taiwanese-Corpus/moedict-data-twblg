beea21f54363067b62ff13708630361234e6a4b7
chhsueh <ihcaod@gmail.com>
2014-04-08 12:56:31 +0800

    改用json輸出 丞宏

--- test/test3/產生閩南語國語對照表.py
+++ test/test3/產生閩南語國語對照表.py
@@ -3,9 +3,10 @@ import xdrlib , sys
 import xlrd
 import time
 from html.parser import HTMLParser
+import json
 
 class handle_excel():
-	def open_excel(self,file):
+	def open_excel(self, file):
 		try:
 				data = xlrd.open_workbook(file)
 				return data
@@ -24,23 +25,23 @@ class handle_excel():
 				if row:
 					for i in range(len(colnames)):
 						a = set(row[i])
-						b = a|b															
+						b = a | b															
 		return b
 
 	def excel_table_for_編號(self, file, colnameindex=0, by_index=0):
 		a = []	
 		data = self.open_excel(file)
 		table = data.sheets()[by_index]
-		nrows = table.nrows# 行数	
-		colnames = table.row_values(colnameindex)# 某一行数据			
+		nrows = table.nrows  # 行数	
+		colnames = table.row_values(colnameindex)  # 某一行数据			
 		for rownum in range(1, nrows):
 				row = table.row_values(rownum)
-				a.append( (row[0],row[3]) )			
+				a.append((row[0], row[3]))			
 		return a
 
 
 class MyHTMLParser(HTMLParser):
-	#無法度確定什麼時候會初使化，資料盡量要歸零，所以丟給main維護
+	# 無法度確定什麼時候會初使化，資料盡量要歸零，所以丟給main維護
 	def 初使化(self):
 		self.output = []
 		self.counter = 0 
@@ -51,13 +52,13 @@ class MyHTMLParser(HTMLParser):
 			
 class 資料處理():
 	excel = handle_excel()
-	所有字集 = excel.excel_table_for_字元(r'../twblg_data_20131230/例句.xls')|excel.excel_table_for_字元(r'../twblg_data_20131230/釋義.xls')
+	所有字集 = ['一']  # excel.excel_table_for_字元(r'../twblg_data_20131230/例句.xls')|excel.excel_table_for_字元(r'../twblg_data_20131230/釋義.xls')
 	數字對照表 = excel.excel_table_for_編號(r'../twblg_data_20131230/詞目總檔(含俗諺).xls')	
-	全部國語詞=[]
-	國語詞集合=set()
-	國台字音表=[]
+	全部國語詞 = []
+	國語詞集合 = set()
+	國台字音表 = []
 	
-	def 單字搜尋(self, Word = ""):
+	def 單字搜尋(self, Word=""):
 		for i in self.所有字集:
 			while True:
 					try:
@@ -85,8 +86,7 @@ class 資料處理():
 						self.國語詞集合.add(詞)		
 						print(self.全部國語詞) 
 			if parser.counter == 10:
-					self.單字搜尋(NextWord)			
-										
+					self.單字搜尋(NextWord)							
 	
 	def 單詞搜尋(self):
 		定位a = '<tr class="all_space1">'
@@ -94,7 +94,7 @@ class 資料處理():
 		for i in self.全部國語詞:			
 			x = 0
 			單詞 = urllib.request.quote(i)
-			urlx = "http://twblg.dict.edu.tw/holodict_new/result.jsp?radiobutton=0&limit=20&querytarget=2&sample="+單詞+"&submit.x=42&submit.y=14"
+			urlx = "http://twblg.dict.edu.tw/holodict_new/result.jsp?radiobutton=0&limit=20&querytarget=2&sample=" + 單詞 + "&submit.x=42&submit.y=14"
 			sock = urllib.request.urlopen(urlx)
 			parser = MyHTMLParser(strict=False)
 			parser.初使化()
@@ -102,7 +102,7 @@ class 資料處理():
  
 			for j in sock.read().decode("utf8").split('\n'):
 					j = j.strip()			
-					if j == 定位a or x >0:
+					if j == 定位a or x > 0:
 						x = 1												
 						parser.feed(j)
 						if parser.counter == 3:
@@ -117,18 +117,14 @@ class 資料處理():
 		for i in range(len(self.國台字音表)):
 			for j in range(len(self.數字對照表)):
 					if self.國台字音表[i][3] == self.數字對照表[j][1]:
-						self.國台字音表[i][1] = self.數字對照表[j][0]
-		print(self.國台字音表) 
+						self.國台字音表[i][1] = self.數字對照表[j][0] 
 				
 if __name__ == "__main__":
 	資料 = 資料處理()
 	資料.單字搜尋()			
 	資料.單詞搜尋()	
 	資料.編號() 
-
-
-					
-		
-				
-				
-				
+	輸出檔 = open('結果.json', 'w')
+	print(json.dumps(資料.國台字音表), file=輸出檔)
+	print(json.dumps(資料.國台字音表)[:50])
+	輸出檔.close()
