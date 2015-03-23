c1a1a55e148fd6d12473fb7e55e9d23bd0f5967e
薛丞宏 <ihcaoe@gmail.com>
2015-03-23 22:02:43 +0800

    改用辭典較好維護 丞宏

--- 華語對照表下載/test_華語查對應閩南語.py
+++ 華語對照表下載/test_華語查對應閩南語.py
@@ -6,11 +6,26 @@ class 華語查對應閩南語(TestCase):
 	def setUp(self):
 		self.資料 = 資料處理()
 	def test_一些(self):
-		self.assertEqual(self.資料.單詞搜尋('一些'),
-			[['一些', '1', '一寡', '--tsi̍t-kuá'], ['一些', '2', '寡', 'kuá']])
+		self.assertEqual(self.資料.單詞搜尋('一些'),[{
+				'華語':'一些',
+				'主編號':'1', 
+				'漢字':'一寡', 
+				'音標':'--tsi̍t-kuá'},
+			{
+				'華語':'一些',
+				'主編號':'1', 
+				'漢字':'寡', 
+				'音標':'kuá'}
+			])
 	def test_一些些(self):
-		self.assertEqual(self.資料.單詞搜尋('一些些'),
-			[['一些些', '1', '一寡仔', 'tsi̍t-kuá-á']])
+		self.assertEqual(self.資料.單詞搜尋('一些些'),[
+			{
+				'華語':'一些些', 
+				'主編號':'1', 
+				'漢字': '一寡仔', 
+				'音標':'tsi̍t-kuá-á',
+		 	}
+		])
 				
 if __name__ == "__main__":
 	unittest.main()
