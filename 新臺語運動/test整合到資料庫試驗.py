080fda795f4f8ae43920377b5fa6765fd5543de6
薛丞宏 <ihcaoe@gmail.com>
2015-07-19 18:40:50 +0800

    更新試驗

--- 新臺語運動/test整合到資料庫試驗.py
+++ 新臺語運動/test整合到資料庫試驗.py
@@ -1,19 +1,28 @@
 from unittest.case import TestCase
+from 新臺語運動.整理詞目總檔 import 整理詞目總檔
 
 
-from 新臺語運動.整合到資料庫 import 整合到資料庫
 
 
 class 整合到資料庫試驗(TestCase):
 
     def setUp(self):
-        self.整合 = 整合到資料庫()
+        self.整理詞目總檔 = 整理詞目總檔()
 
     def test_一般詞目總檔(self):
         self.assertEqual(
             len(list(
-                self.整合.詞目總檔(
+                self.整理詞目總檔.詞目總檔(
                     主編碼=1, 屬性=1, 詞目='一', 音讀='tsi̍t'
+                )
+            )),
+            1)
+
+    def test_有腔口詞目總檔(self):
+        self.assertEqual(
+            len(list(
+                self.整理詞目總檔.詞目總檔(
+                    主編碼=1, 屬性=1, 詞目='火雞', 音讀='hué-ke/hé-kue'
                 )
             )),
             2)
@@ -21,7 +30,7 @@ class 整合到資料庫試驗(TestCase):
     def test_有三區詞目總檔(self):
         self.assertEqual(
             len(list(
-                self.整合.詞目總檔(
+                self.整理詞目總檔.詞目總檔(
                     主編碼=1, 屬性=1, 詞目='俞', 音讀='Jû/Lû/Jî'
                 )
             )),
