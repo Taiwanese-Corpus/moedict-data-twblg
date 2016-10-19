5b5821b53c7da36103063fa2afc2867591b73591
薛丞宏 <ihcaoe@gmail.com>
2016-10-19 18:40:28 +0800

    加處理音檔的script

---
+++ 下載臺語教典音檔-官網沓沓掠.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+prog="wget"
+#prog="echo"
+log_path="log"
+
+for i in $(seq -w 1 62000)
+do
+	url="http://twblg.dict.edu.tw/holodict_new/audio/$i.mp3"
+	$prog -r $url 2>> $log_path
+	echo $url
+	declare -i sec=$RANDOM%2+1
+	sleep $sec;
+done
+
